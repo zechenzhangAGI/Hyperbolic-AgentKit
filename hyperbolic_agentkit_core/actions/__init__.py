b23643b091a08d5f0ecf290840021e4eec2d65d7
Amr Elhady <theamrelhady@gmail.com>
2024-12-23 12:52:13 -0500

    added agent capability to terminate instance

--- hyperbolic_agentkit_core/actions/__init__.py
+++ hyperbolic_agentkit_core/actions/__init__.py
@@ -4,6 +4,7 @@ from hyperbolic_agentkit_core.actions.get_available_gpus import GetAvailableGpus
 from hyperbolic_agentkit_core.actions.get_gpu_status import GetGpuStatusAction
 from hyperbolic_agentkit_core.actions.ssh_access import SSHAccessAction
 from hyperbolic_agentkit_core.actions.remote_shell import RemoteShellAction
+from hyperbolic_agentkit_core.actions.terminate_compute import TerminateComputeAction
 
 
 # WARNING: All new HyperbolicAction subclasses must be imported above, otherwise they will not be discovered
@@ -20,5 +21,5 @@ HYPERBOLIC_ACTIONS = get_all_hyperbolic_actions()
 
 __all__ = [
     "HYPERBOLIC_ACTIONS", "HyperbolicAction", "RentComputeAction", "GetAvailableGpusAction",
-    "GetGpuStatusAction", "SSHAccessAction", "RemoteShellAction"
+    "GetGpuStatusAction", "SSHAccessAction", "RemoteShellAction", "TerminateComputeAction",
 ]
